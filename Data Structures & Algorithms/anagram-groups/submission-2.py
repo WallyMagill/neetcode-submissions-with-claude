844927d00ceff9925bd70ae5b8651e9b58eeea5b
Walter Magill <wallymagill15@gmail.com>
2026-03-29 19:30:40 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = collections.defaultdict(list)
+        for s in strs:
+            groups[''.join(sorted(s))].append(s)
+        ans = []
+        for g in groups:
+            temp = list(groups[g])
+            ans.append(temp)
+        return ans
