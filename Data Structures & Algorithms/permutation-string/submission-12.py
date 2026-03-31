5188eccc5bdcd5eaffb8d005d7321997779226dc
Walter Magill <wallymagill15@gmail.com>
2026-03-31 09:29:24 -0400

    Add: permutation-string - submission-12

---
+++ Data Structures & Algorithms/permutation-string/submission-12.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if s1 == s2:
+            return True
+        if len(s1) > len(s2):
+            return False
+        p1 = p2 = 0
+        while p2 < len(s2):
+            while s1[p1] == s2[p2]:
+                if p1 == len(s1) - 1:
+                    return True
+                p1 += 1
+                p2 += 1
+            p1 = 0
+            p2 += 1
+        return False
+        
